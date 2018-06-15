6e5eade4b4ced7f1090e49ddef2d7b5f954459a1
jg <wujungao@users.noreply.github.com>
2018-06-15 16:26:35 +0800

    Update NetworkInterfaceManager.m

--- NetworkInterfaceManager.m
+++ NetworkInterfaceManager.m
@@ -87,4 +87,69 @@ static NetworkInterfaceManager *_sharedManager=nil;
     }
 }
 
+-(void)checkNetworkflow{
+    struct ifaddrs *ifa_list = 0, *ifa;
+    if (getifaddrs(&ifa_list) == -1)
+    {
+        return;
+    }
+    
+    uint32_t iBytes     = 0;
+    uint32_t oBytes     = 0;
+    uint32_t allFlow    = 0;
+    uint32_t wifiIBytes = 0;
+    uint32_t wifiOBytes = 0;
+    uint32_t wifiFlow   = 0;
+    uint32_t wwanIBytes = 0;
+    uint32_t wwanOBytes = 0;
+    uint32_t wwanFlow   = 0;
+    struct IF_DATA_TIMEVAL time ;
+    
+    for (ifa = ifa_list; ifa; ifa = ifa->ifa_next)
+    {
+        if (AF_LINK != ifa->ifa_addr->sa_family)
+            continue;
+        
+        if (!(ifa->ifa_flags & IFF_UP) && !(ifa->ifa_flags & IFF_RUNNING))
+            continue;
+        
+        if (ifa->ifa_data == 0)
+            continue;
+        
+        // Not a loopback device.
+        // network flow
+        if (strncmp(ifa->ifa_name, "lo", 2))
+        {
+            struct if_data *if_data = (struct if_data *)ifa->ifa_data;
+            
+            iBytes += if_data->ifi_ibytes;
+            oBytes += if_data->ifi_obytes;
+            allFlow = iBytes + oBytes;
+            time = if_data->ifi_lastchange;
+        }
+        
+        //wifi flow
+        if (!strcmp(ifa->ifa_name, "en0"))
+        {
+            struct if_data *if_data = (struct if_data *)ifa->ifa_data;
+            
+            wifiIBytes += if_data->ifi_ibytes;
+            wifiOBytes += if_data->ifi_obytes;
+            wifiFlow    = wifiIBytes + wifiOBytes;
+            time = if_data->ifi_lastchange;//[NSDate dateWithTimeIntervalSince1970:time.tv_sec];ip租约过期时间？
+        }
+        
+        //3G and gprs flow
+        if (!strcmp(ifa->ifa_name, "pdp_ip0"))
+        {
+            struct if_data *if_data = (struct if_data *)ifa->ifa_data;
+            
+            wwanIBytes += if_data->ifi_ibytes;
+            wwanOBytes += if_data->ifi_obytes;
+            wwanFlow    = wwanIBytes + wwanOBytes;
+        }
+    }
+    freeifaddrs(ifa_list);
+}
+
 @end
