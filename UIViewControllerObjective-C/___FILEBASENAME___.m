e865b77eece534a7bf47c93c47c17fb3e3b58598
jg <wujungao@users.noreply.github.com>
2018-08-23 15:30:55 +0800

    UIView，UIViewController Templates
    UIView，UIViewController Templates.
    templates define usually common "prama mark" and methods

---
+++ UIViewControllerObjective-C/___FILEBASENAME___.m
@@ -0,0 +1,70 @@
+//___FILEHEADER___
+
+#import "___FILEBASENAME___.h"
+
+@interface ___FILEBASENAMEASIDENTIFIER___ ()
+
+@end
+
+@implementation ___FILEBASENAMEASIDENTIFIER___
+
+#pragma mark - viewController init
+
+#pragma mark - viewController lifeCycle
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    [self initConfigs];
+    
+    [self configs];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - init configs
+-(void)initConfigs{
+    
+    //init config views
+    //usually we do views auto layout init configs in initConfigViews
+    
+    [self initConfigViews];
+}
+
+#pragma mark - init config views
+-(void)initConfigViews{
+    // invoke all views init config method
+}
+
+#pragma mark -
+// all subviews init config method at here.
+//example: initConfig+ViewClassName{}
+
+#pragma mark - configs
+-(void)configs{
+    
+    //in this mehod,usually we do views config on basis of data or some status after initConfig views
+}
+
+#pragma mark - init config datas
+-(void)initConfigDatas{
+    
+    //in this method,usually we do request data from internet.
+}
+
+#pragma mark -
+
+#pragma mark - property
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ UIViewObjective-C/___FILEBASENAME___.m
@@ -0,0 +1,46 @@
+//___FILEHEADER___
+
+#import "___FILEBASENAME___.h"
+
+@implementation ___FILEBASENAMEASIDENTIFIER___
+
+
+#pragma mark - init configs
+-(void)initConfigs{
+    
+    //init config views
+    //usually we do views auto layout init configs in initConfigViews
+    
+    [self initConfigViews];
+}
+
+#pragma mark - init config views
+-(void)initConfigViews{
+    // invoke all views init config method
+}
+
+#pragma mark -
+// all subviews init config method at here.
+//example: initConfig+ViewClassName{}
+
+#pragma mark - configs
+-(void)configs{
+    
+    //in this mehod,usually we do views config on basis of data or some status after initConfig views
+}
+
+#pragma mark -
+// all sub config method at here.
+//example: config+ViewClassName{}
+
+#pragma mark - property
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
