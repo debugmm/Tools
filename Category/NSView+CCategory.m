b4be0ac307ca771a1a73d836412af2e923dcf3aa
jg <wujungao@users.noreply.github.com>
2019-04-03 10:58:56 +0800

    调整坐标转换方法
    错误的理解了方法，导致采用loop去执行坐标转换

--- Category/NSView+CCategory.m
+++ Category/NSView+CCategory.m
@@ -12,36 +12,7 @@
 
 - (NSRect)getRectOfViewInScreen{
     
-    NSRect dframe=self.frame;
-    
-    NSView *superV=nil;
-    NSView *subV=nil;
-    
-    subV=self.superview;
-    superV=subV.superview;
-    BOOL isContinue=YES;
-    
-    //这个循环的作用，用来将当前的View.frame，一直转换到window.contentView rect
-    //最后，在把从contentView转换的rect，转换成screen上的rect
-    
-    //经过试验：中间view层级特别多，直接转换会有问题
-    //因此，一级一级的转换rect
-    do {
-        
-        if(superV!=nil && subV!=nil){
-            
-            dframe=[subV convertRect:dframe toView:superV];
-            
-            subV=superV;
-            superV=subV.superview;
-            
-            continue;
-        }
-        
-        isContinue=NO;
-        
-    } while (isContinue);
-    
+    NSRect dframe=[self convertRect:self.bounds toView:nil];
     dframe=[self.window convertRectToScreen:dframe];
     
     return dframe;
